86843953f1391e11886f325d5d5f635fcbb90137
chinenyeugwu <128453858+chinenyeugwu@users.noreply.github.com>
2025-04-11 01:52:54 +0700

    updated

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="border-t py-4">
         <div className="container mx-auto px-4">
           <p className="text-center text-sm text-muted-foreground">
-            © {new Date().getFullYear()} Demo. All rights reserved.
+            © {new Date().getFullYear()} Rectifier. All rights reserved.
           </p>
         </div>
       </footer>
